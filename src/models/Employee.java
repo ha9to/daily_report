10eaf50bee8772274b707a0cc3355cca45eae6e7
ha9to <adam.game.play@yahoo.co.jp>
2020-10-28 21:15:42 +0900

    Add Employee index

--- src/models/Employee.java
+++ src/models/Employee.java
@@ -11,52 +11,51 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-@Table(name="employees")
+@Table(name = "employees")
 @NamedQueries({
     @NamedQuery(
-            name="getAllEmployees",
-            query="select e from Employee as e order by e.id desc"
+            name = "getAllEmployees",
+            query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
             ),
     @NamedQuery(
-            name="getEmployeesCount",
-            query="select count(e) from Employee as e"
+            name = "getEmployeesCount",
+            query = "SELECT COUNT(e) FROM Employee AS e"
             ),
     @NamedQuery(
-            name="checkRegisteredCode",
-            query="select count(e) from Employee as e where e.code=:code"
+            name = "checkRegisteredCode",
+            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
             ),
     @NamedQuery(
-            name="checkLoginCodeAndPassword",
-            query="select e from Employee as e where e.delete_flag=0 and e.code=:code and e.password=:pass"
+            name = "checkLoginCodeAndPassword",
+            query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
             )
 })
-
 @Entity
 public class Employee {
     @Id
-    @Column(name="id")
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="code",nullable=false,unique=true)
+    @Column(name = "code", nullable = false, unique = true)
     private String code;
 
-    @Column(name="name",nullable=false)
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name="password",length=64,nullable=false)
+    @Column(name = "password", length = 64, nullable = false)
     private String password;
 
-    @Column(name="admin_flag",nullable=false)
+    @Column(name = "admin_flag", nullable = false)
     private Integer admin_flag;
 
-    @Column(name="created_at",nullable=false)
+    @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
-    @Column(name="updated_at",nullable=false)
+    @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
 
-    @Column(name="delete_flag",nullable=false)
+    @Column(name = "delete_flag", nullable = false)
     private Integer delete_flag;
 
     public Integer getId() {
